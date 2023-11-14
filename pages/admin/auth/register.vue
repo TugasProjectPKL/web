905a861fa4ca6972a3fa555436fd0dc179d94be3
agus <aguspriyo523@gmail.com>
2023-11-14 23:03:31 +0700

    ini registrasi dan forgot password

---
+++ pages/admin/auth/register.vue
@@ -0,0 +1,138 @@
+<script setup lang="ts">
+import logo from "@/assets/materio/images/logo.svg?raw";
+
+const form = ref({
+  username: '',
+  email: '',
+  password: '',
+  privacyPolicies: false,
+});
+
+const isPasswordVisible = ref(false)
+</script>
+
+<template>
+    <v-container>    
+        <v-row justify="center">
+            <v-col cols="12" sm="1" md="2" lg="2">
+
+            </v-col>
+            <v-col cols="12" sm="6" md="6" lg="6">
+                <VCard class="auth-card pa-4 pt-7" max-width="448">
+      <VCardItem class="justify-center">
+        <template #prepend>
+          <div class="d-flex">
+            <div v-html="logo" />
+          </div>
+        </template>
+
+        <VCardTitle class="font-weight-semibold text-2xl text-uppercase">
+          REGISTRASI
+        </VCardTitle>
+      </VCardItem>
+
+      <VCardText class="pt-2">
+        <p class="mb-0">
+        </p>
+      </VCardText>
+
+      <VCardText>
+        <VForm @submit.prevent="() => {}">
+          <VRow>
+            <!-- Username -->
+            <VCol cols="12">
+              <VTextField
+                v-model="form.username"
+                label="Username"
+              />
+            </VCol>
+            <!-- email -->
+            <VCol cols="12">
+              <VTextField
+                v-model="form.email"
+                label="Email"
+                type="email"
+              />
+            </VCol>
+
+            <!-- password -->
+            <VCol cols="12">
+              <VTextField
+                v-model="form.password"
+                label="Password"
+                :type="isPasswordVisible ? 'text' : 'password'"
+                :append-inner-icon="isPasswordVisible ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
+                @click:append-inner="isPasswordVisible = !isPasswordVisible"
+              />
+              <div class="d-flex align-center mt-1 mb-4">
+                <VCheckbox id="privacy-policy" v-model="form.privacyPolicies" inline/>
+                <VLabel
+                  for="privacy-policy"
+                  style="opacity: 1;"
+                >
+                  <span class="me-1">I agree to</span>
+                  <a
+                    href="javascript:void(0)"
+                    class="text-primary"
+                  >privacy policy & terms</a>
+                </VLabel>
+              </div>
+
+              <VBtn
+                block
+                type="submit"
+                to="/"
+              >
+                Sign up
+              </VBtn>
+            </VCol>
+
+            <!-- login instead -->
+            <VCol
+              cols="12"
+              class="text-center text-base"
+            >
+              <span>Already have an account?</span>
+              <RouterLink
+                class="text-primary ms-2"
+                to="login"
+              >
+                Sign in instead
+              </RouterLink>
+            </VCol>
+
+            <VCol
+              cols="12"
+              class="d-flex align-center"
+            >
+              <VDivider />
+              <span class="mx-4">or</span>
+              <VDivider />
+            </VCol>
+
+            <!-- auth providers -->
+            <VCol
+              cols="12"
+              class="text-center"
+            >
+              <AuthProvider />
+            </VCol>
+          </VRow>
+        </VForm>
+      </VCardText>
+    </VCard>
+    </v-col>
+        <v-col cols="12" sm="1" md="2" lg="2">
+                
+        </v-col>
+    </v-row>
+</v-container>
+</template>
+
+<style lang="scss">
+@use "@/assets/scss/pages/page-auth.scss";
+
+.layout-wrapper.layout-blank{
+    flex-direction: column;
+}
+</style>
