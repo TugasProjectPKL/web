5249eed22c330aec884b452c36789fa640f42cfe
nurhuda <nurhuda121001@gmail.com>
2023-11-30 23:38:52 +0700

    acc mas ryan

--- components/MainNavbar.vue
+++ components/MainNavbar.vue
@@ -1,72 +1,70 @@
-<script setup>
-const user = useSupabaseUser();
-const router = useRouter();
-function profile() {
-  if (user.value) {
-    router.push({ path: "/profile" });
-  } else {
-    router.push({ path: "/auth/login" });
+  <script setup>
+  const user = useSupabaseUser();
+  const router = useRouter();
+  function profile() {
+    if (user.value) {
+      router.push({ path: "/profile" });
+    } else {
+      router.push({ path: "/auth/login" });
+    }
   }
-}
-</script>
-<template>
-  <nav
-    class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark"
-    arial-label="Furni navigation bar"
-  >
-    <div class="container">
-      <NuxtLink class="navbar-brand" to="/"
-        >PT. Starindo Multi Industri<span>.</span></NuxtLink
-      >
+  </script>
+  <template>
+    <nav
+      class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark"
+      arial-label="Furni navigation bar"
+    >
+      <div class="container">
+        <NuxtLink class="navbar-brand" to="/"
+          >PT. Starindo Multi Industri<span>.</span></NuxtLink
+        >
 
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarsFurni"
-        aria-controls="navbarsFurni"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarsFurni"
+          aria-controls="navbarsFurni"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarsFurni">
-        <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-          <li class="nav-item active">
-            <NuxtLink class="nav-link" to="/">Home</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink class="nav-link" to="/shop/shop">Shop</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink class="nav-link" to="/customerservice/cs"
-              >Customer Service</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink class="nav-link" to="/about">About us</NuxtLink>
-          </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarsFurni">
+          <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+            <li :class="{ 'nav-item': true, 'active': $route.path === '/' }">
+              <NuxtLink class="nav-link" to="/" exact-active-class="active">Home</NuxtLink>
+            </li>
+            <li :class="{ 'nav-item': true, 'active': $route.path.includes('/shop/shop') }">
+              <NuxtLink class="nav-link" to="/shop/shop" exact-active-class="active">Shop</NuxtLink>
+            </li>
+            <li :class="{ 'nav-item': true, 'active': $route.path.includes('/customer_service/cs') }">
+              <NuxtLink class="nav-link" to="/customer_service/cs" exact-active-class="active">Customer Service</NuxtLink>
+            </li>
+            <li :class="{ 'nav-item': true, 'active': $route.path === '/about' }">
+              <NuxtLink class="nav-link" to="/about" exact-active-class="active">About us</NuxtLink>
+            </li>
+          </ul>
 
-        <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-          <li>
-            <a class="nav-link" @click="profile">
-              <img src="@/assets/images/user.svg" />
-            </a>
-          </li>
-          <li>
-            <NuxtLink class="nav-link" to="/checkout/chart">
-              <img src="@/assets/images/cart.svg" />
-            </NuxtLink>
-          </li>
-        </ul>
+          <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+            <li>
+              <a class="nav-link" @click="profile">
+                <img src="@/assets/images/user.svg" />
+              </a>
+            </li>
+            <li>
+              <NuxtLink class="nav-link" to="/checkout/chart">
+                <img src="@/assets/images/cart.svg" />
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-</template>
-<style scoped>
-@import url("@/assets/css/bootstrap.min.css");
-@import url("@/assets/css/style.css");
-@import url("@/assets/css/tiny-slider.css");
-</style>
+    </nav>
+  </template>
+  <style scoped>
+  @import url("@/assets/css/bootstrap.min.css");
+  @import url("@/assets/css/style.css");
+  @import url("@/assets/css/tiny-slider.css");
+  </style>
--- pages/shop/shop.vue
+++ pages/shop/shop.vue
@@ -27,124 +27,31 @@
             <div class="container">
                 <div class="row">
 
-                    <!-- Start Column 1 -->
-                        <div class="col-12 col-md-4 col-lg-3 mb-5">
-                            <a class="product-item" href="#">
-                                <img src="~/assets/images/1.png" class="img-fluid product-thumbnail">
-                                    <h3 class="product-title">
-                                        Nordic Chair
-                                    </h3>
-                                <strong class="product-price">$50.00</strong>
-
-                                <span class="icon-cross">
-                                <img src="~/assets/images/cross.svg" class="img-fluid">
-                                </span>
-                            </a>
-                        </div> 
-                    <!-- End Column 1 -->
+                   
                         
-                    <!-- Start Column 2 -->
-                    <div class="col-12 col-md-4 col-lg-3 mb-5">
-                        <a class="product-item" href="#">
-                            <img src="~/assets/images/2.png" class="img-fluid product-thumbnail">
-                                <h3 class="product-title">
-                                    Nordic Chair
-                                </h3>
-                            <strong class="product-price">$50.00</strong>
+                    
+                    <ItemProductHome
+          v-for="item in product.dataProductDashboard"
+          :image="item.product_image ?? ''"
+          :name="item.product_name ?? ''"
+          :price="item.product_price ?? 0"
+          @add="
+            () => {
+              product.insertProductToCart(item.id);
+            }
+          "
+        />
+                    
 
-                            <span class="icon-cross">
-                                <img src="~/assets/images/cross.svg" class="img-fluid">
-                            </span>
-                        </a>
-                    </div> 
-                    <!-- End Column 2 -->
-
-                    <!-- Start Column 3 -->
-                    <div class="col-12 col-md-4 col-lg-3 mb-5">
-                        <a class="product-item" href="#">
-                            <img src="~/assets/images/3.png" class="img-fluid product-thumbnail">
-                                <h3 class="product-title">
-                                    Kruzo Aero Chair
-                                </h3>
-                            <strong class="product-price">$78.00</strong>
-
-                            <span class="icon-cross">
-                                <img src="~/assets/images/cross.svg" class="img-fluid">
-                            </span>
-                        </a>
-                    </div>
-                    <!-- End Column 3 -->
-
-                    <!-- Start Column 4 -->
-                    <div class="col-12 col-md-4 col-lg-3 mb-5">
-                        <a class="product-item" href="#">
-                            <img src="~/assets/images/4.png" class="img-fluid product-thumbnail">
-                            <h3 class="product-title">Ergonomic Chair</h3>
-                            <strong class="product-price">$43.00</strong>
-
-                            <span class="icon-cross">
-                                <img src="~/assets/images/cross.svg" class="img-fluid">
-                            </span>
-                        </a>
-                    </div>
-                    <!-- End Column 4 -->
+                    
 
 
-                    <!-- Start Column 1 -->
-                    <div class="col-12 col-md-4 col-lg-3 mb-5">
-                        <a class="product-item" href="#">
-                            <img src="~/assets/images/5.png" class="img-fluid product-thumbnail">
-                            <h3 class="product-title">Nordic Chair</h3>
-                            <strong class="product-price">$50.00</strong>
-
-                            <span class="icon-cross">
-                                <img src="~/assets/images/cross.svg" class="img-fluid">
-                            </span>
-                        </a>
-                    </div> 
-                    <!-- End Column 1 -->
+                    
                         
-                    <!-- Start Column 2 -->
-                    <div class="col-12 col-md-4 col-lg-3 mb-5">
-                        <a class="product-item" href="#">
-                            <img src="~/assets/images/6.png" class="img-fluid product-thumbnail">
-                            <h3 class="product-title">Nordic Chair</h3>
-                            <strong class="product-price">$50.00</strong>
+                    
+                    
 
-                            <span class="icon-cross">
-                                <img src="~/assets/images/cross.svg" class="img-fluid">
-                            </span>
-                        </a>
-                    </div> 
-                    <!-- End Column 2 -->
-
-                    <!-- Start Column 3 -->
-                    <div class="col-12 col-md-4 col-lg-3 mb-5">
-                        <a class="product-item" href="#">
-                            <img src="~/assets/images/7.png" class="img-fluid product-thumbnail">
-                            <h3 class="product-title">Kruzo Aero Chair</h3>
-                            <strong class="product-price">$78.00</strong>
-
-                            <span class="icon-cross">
-                                <img src="~/assets/images/cross.svg" class="img-fluid">
-                            </span>
-                        </a>
-                    </div>
-                    <!-- End Column 3 -->
-
-                    <!-- Start Column 4 -->
-                    <div class="col-12 col-md-4 col-lg-3 mb-5">
-                        <a class="product-item" href="#">
-                            <img src="~/assets/images/8.png" class="img-fluid product-thumbnail">
-                            <h3 class="product-title">Ergonomic Chair</h3>
-                            <strong class="product-price">$43.00</strong>
-
-                            <span class="icon-cross">
-                                <img src="~/assets/images/cross.svg" class="img-fluid">
-                            </span>
-                        </a>
-                    </div>
-                    <!-- End Column 4 -->
+                   
 
                 </div>
             </div>
