14ebec953ad93127d5991264320c7f536b0edfae
agus <aguspriyo523@gmail.com>
2023-12-07 22:01:54 +0700

    tentang kami

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,76 @@
 <template>
   <MainNavbar />
-  <MainFooter />
+  <div class="intro-excerpt">
+    <div class="centered-container">
+      <br/>
+      <div style="text-align: center;">
+            <h3>TENTANG KAMI</h3>
+            <p class="centered-text">
+              Dengan pengalaman lebih dari 30 tahun yang kami miliki di industri pengemasan, kami berhasil menciptakan budaya manufaktur yang inovatif, sehingga memungkingkan kami untuk mencapai efisiensi terbesar yang mendorong keunggulan kompetitif kami.
+            </p>
+            <p>
+              Seiring dengan peminat dan permintaan pasar Indonesia yang berkembang dengan pesat, bersamaan dengan bertambahnya pengguna e-commerce diseluruh Indonesia, maka didirikanlah PT. Starindo Multi Industri, dengan tujuan memberikan solusi terbaik di pasar pita perekat Indonesia dalam menghemat biaya bagi pelanggan untuk mendapatkan jaminan kualitas dengan biaya seminimal mungkin.
+            </p>
+            <p>
+              Di Starindo, kami percaya bahwa bisnis adalah tentang produk dan relasi. Selain produk-produk yang bisa diandalkan, kami juga akan berusaha untuk memberikan pelayanan terbaik untuk kebutuhan pelanggan dengan harapan dapat menjalin hubungan yang panjang dan terus-menerus.
+            </p> 
+            <p>
+              Di Starindo, kami adalah perusahaan dengan penuh integritas, bertanggung jawab dan responsif, mampu memberikan pelayanan dan kualitas terbaik, dan memiliki kemampuan untuk beradaptasi seiring dengan perkembangan era.
+            </p>
+            <br/>
+            <br/>
+        </div>
+     </div>
+  </div>
+  <br/>
+  <MainFooter>
+  <div class="sofa-img">
+        <img src="@/assets/images/people.png" alt="Image" class="img-fluid" />
+      </div>
+
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="subscription-form">
+            <h3 class="d-flex align-items-center">
+              <span class="me-1"
+                ><img
+                  src="@/assets/images/envelope-outline.svg"
+                  alt="Image"
+                  class="img-fluid" /></span
+              ><span>Subscribe to Newsletter</span>
+            </h3>
+
+            <form action="#" class="row g-3">
+              <div class="col-auto">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Enter your name"
+                />
+              </div>
+              <div class="col-auto">
+                <input
+                  type="email"
+                  class="form-control"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div class="col-auto">
+                <button class="btn btn-primary">
+                  <font-awesome-icon icon="fa-regular fa-paper-plane" />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+  </MainFooter>
 </template>
+
+<style>
+@import url("@/assets/css/bootstrap.min.css");
+
+/* Pastikan import style yang diperlukan */
+@import url("~/assets/css/style.css");
+@import url("~/assets/css/tiny-slider.css");
+</style>
