5585d5d9adc8b6d022fd3933bf33732523c37ace
agus <aguspriyo523@gmail.com>
2023-12-13 00:40:38 +0700

    visi & misi

--- pages/checkout/checkout.vue
+++ pages/checkout/checkout.vue
@@ -6,34 +6,7 @@ const route = useRoute()
 </script>
 
 <template>
-		<!-- Start Header/Navigation -->
-		<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
-
-			<div class="container">
-				<a class="navbar-brand" href="index.html">Furni<span>.</span></a>
-
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div class="collapse navbar-collapse" id="navbarsFurni">
-					<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-						<li class="nav-item ">
-							<a class="nav-link" href="index.html">Home</a></li>
-							<li>
-							<a class="nav-link" href="shop.html">Shop</a></li>
-					</ul>
-
-					<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-						<li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
-						<li><a class="nav-link" href="cart.html"><img src="images/cart.svg"></a></li>
-					</ul>
-				</div>
-			</div>
-				
-		</nav>
-		<!-- End Header/Navigation -->
-
+  <MainNavbar />
 		<!-- Start Hero Section -->
 			<div class="hero">
 				<div class="container">
@@ -301,113 +274,54 @@ const route = useRoute()
 		    </div>
 		  </div>
 
-		<!-- Start Footer Section -->
-		<footer class="footer-section">
-			<div class="container relative">
+		  <MainFooter>
+  <div class="sofa-img">
+        <img src="@/assets/images/people.png" alt="Image" class="img-fluid" />
+      </div>
 
-				<div class="sofa-img">
-					<img src="images/sofa.png" alt="Image" class="img-fluid">
-				</div>
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="subscription-form">
+            <h3 class="d-flex align-items-center">
+              <span class="me-1"
+                ><img
+                  src="@/assets/images/envelope-outline.svg"
+                  alt="Image"
+                  class="img-fluid" /></span
+              ><span>Subscribe to Newsletter</span>
+            </h3>
 
-				<div class="row">
-					<div class="col-lg-8">
-						<div class="subscription-form">
-							<h3 class="d-flex align-items-center"><span class="me-1"><img src="images/envelope-outline.svg" alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
-
-							<form action="#" class="row g-3">
-								<div class="col-auto">
-									<input type="text" class="form-control" placeholder="Enter your name">
-								</div>
-								<div class="col-auto">
-									<input type="email" class="form-control" placeholder="Enter your email">
-								</div>
-								<div class="col-auto">
-									<button class="btn btn-primary">
-										<span class="fa fa-paper-plane"></span>
-									</button>
-								</div>
-							</form>
-
-						</div>
-					</div>
-				</div>
-
-				<div class="row g-5 mb-5">
-					<div class="col-lg-4">
-						<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
-						<p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
-
-						<ul class="list-unstyled custom-social">
-							<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
-							<li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
-							<li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
-							<li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
-						</ul>
-					</div>
-
-					<div class="col-lg-8">
-						<div class="row links-wrap">
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">About us</a></li>
-									<li><a href="#">Services</a></li>
-									<li><a href="#">Blog</a></li>
-									<li><a href="#">Contact us</a></li>
-								</ul>
-							</div>
-
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">Support</a></li>
-									<li><a href="#">Knowledge base</a></li>
-									<li><a href="#">Live chat</a></li>
-								</ul>
-							</div>
-
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">Jobs</a></li>
-									<li><a href="#">Our team</a></li>
-									<li><a href="#">Leadership</a></li>
-									<li><a href="#">Privacy Policy</a></li>
-								</ul>
-							</div>
-
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">Nordic Chair</a></li>
-									<li><a href="#">Kruzo Aero</a></li>
-									<li><a href="#">Ergonomic Chair</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-
-				</div>
-
-				<div class="border-top copyright">
-					<div class="row pt-4">
-						<div class="col-lg-6">
-                            <p class="mb-2 text-center text-lg-start">Copyright &copy; date here All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> Distributed By <a hreff="https://themewagon.com">ThemeWagon</a>  <!-- License information: https://untree.co/license/ -->
-      </p>
-						</div>
-
-						<div class="col-lg-6 text-center text-lg-end">
-							<ul class="list-unstyled d-inline-flex ms-auto">
-								<li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-								<li><a href="#">Privacy Policy</a></li>
-							</ul>
-						</div>
-
-					</div>
-				</div>
-
-			</div>
-		</footer>
-		<!-- End Footer Section -->	
+            <form action="#" class="row g-3">
+              <div class="col-auto">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Enter your name"
+                />
+              </div>
+              <div class="col-auto">
+                <input
+                  type="email"
+                  class="form-control"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div class="col-auto">
+                <button class="btn btn-primary">
+                  <font-awesome-icon icon="fa-regular fa-paper-plane" />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+  </MainFooter>
 </template>
-<style scoped>
+
+<style>
 @import url("@/assets/css/bootstrap.min.css");
-@import url("@/assets/css/style.css");
-@import url("@/assets/css/tiny-slider.css");
+
+/* Pastikan import style yang diperlukan */
+@import url("~/assets/css/style.css");
+@import url("~/assets/css/tiny-slider.css");
 </style>
---
+++ pages/customerservice/cs.vue
@@ -0,0 +1,349 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "dashboard",
+});
+const route = useRouter();
+</script>
+
+<template>
+  <!-- Start Header/Navigation -->
+  <!-- End Header/Navigation -->
+  <MainNavbar />
+  <!-- Start Hero Section -->
+  <div class="hero">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-lg-5">
+          <div class="intro-excerpt">
+            <h1>Customer Service</h1>
+          </div>
+        </div>
+        <div class="col-lg-7"></div>
+      </div>
+    </div>
+  </div>
+  <!-- End Hero Section -->
+
+  <div class="untree_co-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 mb-5 mb-md-0">
+          <h2 class="h3 mb-3 text-black">Form Komplain</h2>
+          <div class="p-3 p-lg-5 border bg-white">
+            <div class="form-group row">
+              <div class="col-md-6">
+                <label for="c_fname" class="text-black"
+                  >Nama Depan <span class="text-danger">*</span></label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="c_fname"
+                  name="c_fname"
+                />
+              </div>
+              <div class="col-md-6">
+                <label for="c_lname" class="text-black"
+                  >Nama Belakang <span class="text-danger">*</span></label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="c_lname"
+                  name="c_lname"
+                />
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <div class="col-md-12">
+                <label for="c_address" class="text-black"
+                  >Nomor Invoice <span class="text-danger">*</span></label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="c_address"
+                  name="c_address"
+                  placeholder="Hanya Angka"
+                />
+              </div>
+            </div>
+
+            <div class="form-group row mb-5">
+              <div class="col-md-6">
+                <label for="c_email_address" class="text-black"
+                  >Email <span class="text-danger">*</span></label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="c_email_address"
+                  name="c_email_address"
+                />
+              </div>
+              <div class="col-md-6">
+                <label for="c_phone" class="text-black"
+                  >Phone <span class="text-danger">*</span></label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="c_phone"
+                  name="c_phone"
+                  placeholder="Nomor HP"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="collapse" id="ship_different_address">
+                <div class="py-2">
+                  <div class="form-group">
+                    <label for="c_diff_country" class="text-black"
+                      >Country <span class="text-danger">*</span></label
+                    >
+                    <select id="c_diff_country" class="form-control">
+                      <option value="1">Select a country</option>
+                      <option value="2">bangladesh</option>
+                      <option value="3">Algeria</option>
+                      <option value="4">Afghanistan</option>
+                      <option value="5">Ghana</option>
+                      <option value="6">Albania</option>
+                      <option value="7">Bahrain</option>
+                      <option value="8">Colombia</option>
+                      <option value="9">Dominican Republic</option>
+                    </select>
+                  </div>
+
+                  <div class="form-group row">
+                    <div class="col-md-6">
+                      <label for="c_diff_fname" class="text-black"
+                        >First Name <span class="text-danger">*</span></label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="c_diff_fname"
+                        name="c_diff_fname"
+                      />
+                    </div>
+                    <div class="col-md-6">
+                      <label for="c_diff_lname" class="text-black"
+                        >Last Name <span class="text-danger">*</span></label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="c_diff_lname"
+                        name="c_diff_lname"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="form-group row mb-3">
+                    <div class="col-md-12">
+                      <label for="c_diff_address" class="text-black"
+                        >Address <span class="text-danger">*</span></label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="c_diff_address"
+                        name="c_diff_address"
+                        placeholder="Street address"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Apartment, suite, unit etc. (optional)"
+                    />
+                  </div>
+
+                  <div class="form-group row">
+                    <div class="col-md-6">
+                      <label for="c_diff_state_country" class="text-black"
+                        >State / Country
+                        <span class="text-danger">*</span></label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="c_diff_state_country"
+                        name="c_diff_state_country"
+                      />
+                    </div>
+                    <div class="col-md-6">
+                      <label for="c_diff_postal_zip" class="text-black"
+                        >Posta / Zip <span class="text-danger">*</span></label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="c_diff_postal_zip"
+                        name="c_diff_postal_zip"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="form-group row mb-5">
+                    <div class="col-md-6">
+                      <label for="c_diff_email_address" class="text-black"
+                        >Email Address <span class="text-danger">*</span></label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="c_diff_email_address"
+                        name="c_diff_email_address"
+                      />
+                    </div>
+                    <div class="col-md-6">
+                      <label for="c_diff_phone" class="text-black"
+                        >Phone <span class="text-danger">*</span></label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="c_diff_phone"
+                        name="c_diff_phone"
+                        placeholder="Phone Number"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="c_order_notes" class="text-black"
+                >Catatan untuk penjual</label
+              >
+              <textarea
+                name="c_order_notes"
+                id="c_order_notes"
+                cols="30"
+                rows="5"
+                class="form-control"
+                placeholder="Tulis Catatan anda disini..."
+              ></textarea>
+            </div>
+            <br />
+            <div class="form-group">
+              <button
+                class="btn btn-black btn-lg py-3 btn-block"
+                onclick="window.location='thankyou.html'"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- </form> -->
+    </div>
+  </div>
+
+  <!-- Start Footer Section -->
+  <footer class="footer-section">
+    <div class="container relative">
+      <div class="row g-5 mb-5">
+        <div class="col-lg-4">
+          <div class="mb-4 footer-logo-wrap">
+            <a href="#" class="footer-logo"
+              >Starindo Multi Industri<span>.</span></a
+            >
+          </div>
+          <p class="mb-4">
+            Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis
+            nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate
+            velit imperdiet dolor tempor tristique. Pellentesque habitant
+          </p>
+
+          <ul class="list-unstyled custom-social">
+            <li>
+              <a href="#"><span class="fa fa-brands fa-facebook-f"></span></a>
+            </li>
+            <li>
+              <a href="#"><span class="fa fa-brands fa-twitter"></span></a>
+            </li>
+            <li>
+              <a href="#"><span class="fa fa-brands fa-instagram"></span></a>
+            </li>
+            <li>
+              <a href="#"><span class="fa fa-brands fa-linkedin"></span></a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-8">
+          <div class="row links-wrap">
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact us</a></li>
+              </ul>
+            </div>
+
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Knowledge base</a></li>
+                <li><a href="#">Live chat</a></li>
+              </ul>
+            </div>
+
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Our team</a></li>
+                <li><a href="#">Leadership</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="#">Nordic Chair</a></li>
+                <li><a href="#">Kruzo Aero</a></li>
+                <li><a href="#">Ergonomic Chair</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="border-top copyright">
+        <div class="row pt-4">
+          <div class="col-lg-6">
+            <p class="mb-2 text-center text-lg-start">
+              Copyright &copy; date here All Rights Reserved. &mdash; Designed
+              with love by <a href="https://untree.co">Untree.co</a> Distributed
+              By <a hreff="https://themewagon.com">ThemeWagon</a>
+              <!-- License information: https://untree.co/license/ -->
+            </p>
+          </div>
+
+          <div class="col-lg-6 text-center text-lg-end">
+            <ul class="list-unstyled d-inline-flex ms-auto">
+              <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer Section -->
+</template>
+<style scoped>
+@import url("@/assets/css/bootstrap.min.css");
+@import url("@/assets/css/style.css");
+@import url("@/assets/css/tiny-slider.css");
+</style>
